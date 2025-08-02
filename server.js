f9554264569837a3c7fa7c41bc1813ecec38bf70
Ujjawal Sharma <ujjawalkpa09@gmail.com>
2025-08-02 08:47:47 +0530

    first push

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express') ;
+
+const app = express() ; 
+const PORT = '3000' ; 
+
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
+});
+
+app.listen(PORT , () => {
+     console.log(`🚀 Server is running on http://localhost:${PORT}`);
+})  ;
+
+
+
